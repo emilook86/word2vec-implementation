2d4d7920fe5b0cdc01b4ca462ac8c6d54095fc5f
emilook86 <emil.lasocha9@gmail.com>
2026-03-17 19:27:28 +0100

    add inputs and ouputs from dataset

--- word2vec_implementation.py
+++ word2vec_implementation.py
@@ -5,7 +5,7 @@ class Word2Vec:
     def __init__(
         self,
         dataset: list[str],
-        context_window_size: int = 3,
+        context_window_size: int = 2,
         embedding_dimension: int = 5,
         learning_rate: float = 0.1,
     ):
@@ -16,7 +16,11 @@ class Word2Vec:
         self.learning_rate = learning_rate
 
         self.vocabulary, self.vocabulary_size = self.create_vocabulary()
-        self.embeddings_matrix = self.create_embeddings()
+
+        self.inputs, self.outputs = self.create_input_output()
+
+        self.input_embeddings_matrix = self.create_embeddings()
+        self.output_embeddings_matrix = self.create_embeddings()
 
     @staticmethod
     def process_vocabulary(vocab):
@@ -35,7 +39,7 @@ class Word2Vec:
         vocabulary = set()
         for sentence in self.dataset:
             vocabulary.update(sentence.split())
-        vocabulary = {idx: word for idx, word in enumerate(list(vocabulary))}
+        vocabulary = {word: idx for idx, word in enumerate(list(vocabulary))}
         vocabulary_size = len(vocabulary)
         return vocabulary, vocabulary_size
 
@@ -48,7 +52,33 @@ class Word2Vec:
         )
         return embeddings_matrix
 
-    def forward(self):
+    def create_input_output(self):
+        inputs = []
+        outputs = []
+        for sentence in self.dataset:
+            words = sentence.split()
+            sentence_length = len(words)
+            if sentence_length < 2 * self.context_window_size + 1:
+                continue
+            for idx in range(
+                self.context_window_size, sentence_length - self.context_window_size
+            ):
+                input_vector = np.zeros(self.vocabulary_size)
+                output_vector = np.zeros(self.vocabulary_size)
+                output_vector[self.vocabulary[words[idx]]] += 1
+                for offset in range(1, self.context_window_size + 1):
+                    input_vector[self.vocabulary[words[idx - offset]]] += 1
+                    input_vector[self.vocabulary[words[idx + offset]]] += 1
+                inputs.append(input_vector)
+                outputs.append(output_vector)
+        inputs = np.array(inputs)
+        outputs = np.array(outputs)
+        return inputs, outputs
+
+    def forward(self, input):
+        pass
+
+    def loss(self, y_true, y_pred):
         pass
 
     def backpropagation(self):
@@ -56,3 +86,15 @@ class Word2Vec:
 
     def train_step(self):
         pass
+
+
+if __name__ == "__main__":
+    dataset = [
+        "Hello there! What a beautiful day outside",
+        "This photo is really beautiful",
+        "Is that a bird? Is that a plane? No, that's Superman!",
+    ]
+    w2v = Word2Vec(dataset)
+    print(w2v.inputs)
+    print(w2v.outputs)
+    print(w2v.vocabulary)
