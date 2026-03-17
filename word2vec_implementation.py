ecfb177c2b37fa8fc276064610466c983ea02b98
emilook86 <emil.lasocha9@gmail.com>
2026-03-17 17:00:04 +0100

    add basic initialization of word2vec class

--- word2vec_implementation.py
+++ word2vec_implementation.py
@@ -1,19 +1,46 @@
-import numpy
+import numpy as np
 
 
 class Word2Vec:
     def __init__(
         self,
-        vocabulary_length: int = 10,
+        dataset: list[str],
         context_window_size: int = 3,
         embedding_dimension: int = 5,
         learning_rate: float = 0.1,
     ):
 
-        self.vocabulary_length = vocabulary_length
+        self.dataset = dataset
         self.context_window_size = context_window_size
         self.embedding_dimension = embedding_dimension
         self.learning_rate = learning_rate
+
+        self.vocabulary, self.vocabulary_size = self.create_vocabulary()
+        self.embeddings_matrix = self.create_embeddings()
+
+    def create_vocabulary(self):
+        vocabulary = set()
+        for sentence in self.dataset:
+            line = sentence.split(" ")
+            for word in line:
+                word = "".join(char for char in word if char.isalpha())
+                word = word.lower()
+                vocabulary.add(word)
+        vocabulary = list(vocabulary)
+        vocabulary_size = len(vocabulary)
+        return vocabulary, vocabulary_size
+
+    def create_embeddings(self):
+        max_value = np.sqrt(1 / self.vocabulary_size)
+        embeddings_matrix = []
+        for _ in range(self.vocabulary_size):
+            embeddings_matrix.append(
+                np.random.uniform(
+                    low=-max_value, high=max_value, size=self.embedding_dimension
+                )
+            )
+        embeddings_matrix = np.array(embeddings_matrix)
+        return embeddings_matrix
 
     def forward(self):
         pass
@@ -23,3 +50,9 @@ class Word2Vec:
 
     def train_step(self):
         pass
+
+
+vocabulary = set()
+
+
+print(vocabulary)
