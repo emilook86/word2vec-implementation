bd2adf73f08c50d3de813b662d2b28769a81723c
emilook86 <emil.lasocha9@gmail.com>
2026-03-17 18:04:37 +0100

    add dataset processing

--- word2vec_implementation.py
+++ word2vec_implementation.py
@@ -10,7 +10,7 @@ class Word2Vec:
         learning_rate: float = 0.1,
     ):
 
-        self.dataset = dataset
+        self.dataset = self.process_vocabulary(dataset)
         self.context_window_size = context_window_size
         self.embedding_dimension = embedding_dimension
         self.learning_rate = learning_rate
@@ -18,28 +18,34 @@ class Word2Vec:
         self.vocabulary, self.vocabulary_size = self.create_vocabulary()
         self.embeddings_matrix = self.create_embeddings()
 
+    @staticmethod
+    def process_vocabulary(vocab):
+        processed_vocab = []
+        for sentence in vocab:
+            words = sentence.split()
+            processed_words = []
+            for word in words:
+                clean_word = "".join(char for char in word if char.isalpha())
+                if clean_word:
+                    processed_words.append(clean_word.lower())
+            processed_vocab.append(" ".join(processed_words))
+        return processed_vocab
+
     def create_vocabulary(self):
         vocabulary = set()
         for sentence in self.dataset:
-            line = sentence.split(" ")
-            for word in line:
-                word = "".join(char for char in word if char.isalpha())
-                word = word.lower()
-                vocabulary.add(word)
-        vocabulary = list(vocabulary)
+            vocabulary.update(sentence.split())
+        vocabulary = {idx: word for idx, word in enumerate(list(vocabulary))}
         vocabulary_size = len(vocabulary)
         return vocabulary, vocabulary_size
 
     def create_embeddings(self):
         max_value = np.sqrt(1 / self.vocabulary_size)
-        embeddings_matrix = []
-        for _ in range(self.vocabulary_size):
-            embeddings_matrix.append(
-                np.random.uniform(
-                    low=-max_value, high=max_value, size=self.embedding_dimension
-                )
-            )
-        embeddings_matrix = np.array(embeddings_matrix)
+        embeddings_matrix = np.random.uniform(
+            low=-max_value,
+            high=max_value,
+            size=(self.vocabulary_size, self.embedding_dimension),
+        )
         return embeddings_matrix
 
     def forward(self):
@@ -50,9 +56,3 @@ class Word2Vec:
 
     def train_step(self):
         pass
-
-
-vocabulary = set()
-
-
-print(vocabulary)
